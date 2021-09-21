083a2801cf1d50f7c8bd4045ce951dc906bb3d8b
Ganesh Khopade <kpganesh121.gk@gmail.com>
2021-09-21 17:15:00 +0530

    addition.py is modified with value c

--- addtion.py
+++ addtion.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
 
     a = 5
     b = 10
+    c = 20
 
+    def add(a, b, c):
+        return a+b+c
 
-    def add(a, b):
-        return a+b
-
-    c = add(a, b)
-    print (c)
+    d = add(a, b, c)
+    print (d)
 
 
 
